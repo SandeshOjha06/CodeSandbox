0af7b71b421399c2c749640c6a66f05863ee8065
Sandesh <sandeshojha70@gmail.com>
2026-01-07 17:12:12 +0000

    add explorer

---
+++ drizzle/0004_messy_oracle.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "playground" ADD COLUMN "files" text DEFAULT '{}';--> statement-breakpoint
+ALTER TABLE "playground" ADD COLUMN "active_file_id" text;
---
+++ drizzle/0005_big_sir_ram.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "account" RENAME COLUMN "userId" TO "user_id";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "providerAccountId" TO "provider_account_id";--> statement-breakpoint
+ALTER TABLE "account" DROP CONSTRAINT "account_userId_user_id_fk";
+--> statement-breakpoint
+ALTER TABLE "account" DROP CONSTRAINT "account_provider_providerAccountId_pk";--> statement-breakpoint
+ALTER TABLE "account" ADD CONSTRAINT "account_provider_provider_account_id_pk" PRIMARY KEY("provider","provider_account_id");--> statement-breakpoint
+ALTER TABLE "account" ADD COLUMN "id" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "account" ADD CONSTRAINT "account_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
