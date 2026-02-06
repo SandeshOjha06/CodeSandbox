395426ca4ed2a0402de6e1517b1e2910efe739e6
Sandesh <sandeshojha70@gmail.com>
2026-02-06 12:07:34 +0000

    add auto sign-in

--- auth.ts
+++ auth.ts
@@ -1,49 +1,48 @@
-// import NextAuth from "next-auth"
 import type { NextAuthOptions } from "next-auth"
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import Google from "next-auth/providers/google"
 import Github from "next-auth/providers/github"
 import { db } from "@/src/db"
+import { user, account, session, verificationToken } from "@/src/db/schema"
 
-export const authOptions = {
-  adapter: DrizzleAdapter(db),
-
+export const authOptions: NextAuthOptions = {
+  adapter: DrizzleAdapter(db, {
+    usersTable: user,
+    accountsTable: account,
+    sessionsTable: session,
+    verificationTokensTable: verificationToken,
+  }),
   session: {
     strategy: "jwt",
   },
-
   providers: [
     Google({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
     Github({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
-
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
         token.id = user.id
       }
-      // console.log(token);
-      
       return token
     },
-
     async session({ session, token }) {
       if (session.user && token.id) {
         session.user.id = token.id as string
       }
-      // console.log(session);
-      
       return session
     },
   },
-
   pages: {
     signIn: "/auth/signin",
+    error: "/auth/signin",
   },
 }
